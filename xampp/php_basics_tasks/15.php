c6d37ad0eb60524dbd855071e11dd55c6fbecf36
Shkodenko V. Taras <podlom@users.noreply.github.com>
2018-04-16 12:10:29 +0300

    Update 15.php
    Предлагаю такое решение в том же стиле, в котором вы начали реализовывать

--- xampp/php_basics_tasks/15.php
+++ xampp/php_basics_tasks/15.php
@@ -1,7 +1,8 @@
 <?php
+
 $a = rand(1,1000);
 $b = rand(1,1000);
 $operator = ['+'=>1, '-'=>2, '*'=>3, '/'=>4, '%'=>5];
-
-print_r($a . array_rand($operator) . $b);
-?>
+$o = array_rand($operator);
+print_r($a . $o . $b) . PHP_EOL;
+eval("echo ' = '; \$r = \$a " . $o . " \$b ; echo \$r;");
